74b43e10cb99fc3cb0bc4c59b3cfc5f239434f6d
Igor Santos <igor.ox13@gmail.com>
2023-02-07 20:10:53 -0300

    formatação decimal e locale

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
+import java.util.Locale;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Olá Mundo!");
-
+		double x = 10.35784;
+		System.out.println(x);
+		System.out.printf("%.2f%n", x); //formatação de decimal, sem especificar o locale usa o padrão do pc!
+		System.out.printf("%.4f%n", x); //imprime 10,3578
+		Locale.setDefault(Locale.US); //setado o local e formatação.
+		System.out.printf("%.2f%n", x); //imprime 10.35
+		
+		
 	}
 
 }

d0a813ec18d8c8f5966f8750c52803bedfe1d94c
Igor Santos <igor.ox13@gmail.com>
2023-03-02 12:52:24 -0300

    exercicio de fixacao

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -5,44 +5,45 @@ public class Product {
 	private String name;
 	private double price;
 	private int quantity;
-	
-	//construtor
+
+	// construtor
 	public Product() {
 	}
-	
+
 	public Product(String name, double price, int quantity) {
 		this.name = name;
 		this.price = price;
 		this.quantity = quantity;
 	}
-	
+
 	public Product(String name, double price) {
 		this.name = name;
 		this.price = price;
 	}
-	
-	//encapsulamento
+
+	// encapsulamento
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public double getPrice() {
 		return price;
 	}
-	
+
 	public void setPrice(double price) {
 		this.price = price;
 	}
-	
+
 	public int getQuantity() {
 		return quantity;
 	}
-	
 
+	// metodos
 	public double totalValueInStock() {
 		return price * quantity;
 	}

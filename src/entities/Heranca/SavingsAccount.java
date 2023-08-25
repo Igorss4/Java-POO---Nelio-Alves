b824e5b44b68cff95be380960acb6058216f1e6f
Igor Santos <igor.ox13@gmail.com>
2023-08-24 21:17:33 -0300

    final

--- src/entities/Heranca/SavingsAccount.java
+++ src/entities/Heranca/SavingsAccount.java
@@ -1,6 +1,6 @@
 package entities.Heranca;
 
-public class SavingsAccount extends Account{
+public final class SavingsAccount extends Account{
 
 	private Double interestRate;
 	
@@ -25,7 +25,7 @@ public class SavingsAccount extends Account{
 	}
 	
 	@Override
-	public void withdraw(double amount) {
+	public final void withdraw(double amount) {
 		balance -= amount;
 	}
 }
